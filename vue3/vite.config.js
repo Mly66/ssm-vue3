3a54f3a6ddd638b36567e128daa879b713909c99
Mly66 <3127328609@qq.com>
2024-12-13 11:39:43 +0800

    upload接口定义

--- vue3/vite.config.js
+++ vue3/vite.config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,7 +24,14 @@ export default defineConfig({
         changeOrigin: true, 
         rewrite: (path) => path.replace(/^\/api/, ""), 
         logLevel: 'debug', 
+      },
+      '/upload': { 
+        target: 'https://tc.mofashi.ltd', // 代理图片上传请求
+        changeOrigin: true, 
+        rewrite: (path) => path.replace(/^\/upload/, "/api/v1/upload"), // 重写路径
+        secure: true, // 如果目标是 HTTPS，建议开启此选项
+        logLevel: 'debug',
       }
     }
   }
-})
+});
